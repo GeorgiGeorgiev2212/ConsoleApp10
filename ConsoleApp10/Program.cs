aa30edf80729fd6b81703058fb61949bd877faa7
Georgi Georgiev <Georgi Georgiev@DESKTOP-9FI4TKH>
2025-02-07 15:27:59 +0200

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,44 @@
+﻿// type variable || void
+
+
+
+//int sum = Add(5, 6);
+//int secondSum = Add(33, 7);
+
+
+//PrintSum(secondSum);
+
+string f = Console.ReadLine();
+string l = Console.ReadLine();
+
+
+
+
+
+   Console.WriteLine (Combine(f, l));
+   Console.WriteLine(Combine("Today is",  "Tuesday"));
+
+void PrintSum(int x)
+{
+    ;
+
+    Console.WriteLine($"Sum is =  {x}");
+
+}
+
+ int Add(int first, int second)
+ {
+
+
+    return first + second;
+
+
+ }
+
+
+string Combine(string first, string second)
+{ 
+    string combinedString = first + second;
+    return combinedString;
+
+}
